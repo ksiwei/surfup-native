ed674a23dcd8e4cfc52e0d056de005a4df00d3b0
Siwei Kang <siweikang@gmail.com>
2017-09-29 23:21:15 +0100

    Added native map

--- Containers/DetailScreen.js
+++ Containers/DetailScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class DetailScreen extends React.Component {
     static navigationOptions = ({ navigation }) => ({
@@ -9,6 +10,38 @@ export default class DetailScreen extends React.Component {
     render() {
       const { params } = this.props.navigation.state;
       
-      return <View><Text>Explore {params.location}</Text></View>
+      return (
+        <View style={styles.container}>
+            <Text>Explore {params.location}</Text>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+                }}
+            />
+        </View>
+      )
     }
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+  })
